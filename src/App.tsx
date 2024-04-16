48945b58598c26d54eb2f2013118a615568248d0
Håkon <Haksto@haugnett.no>
2024-04-16 12:47:59 +0200

    fixed buttons with flex, and made it look cleaner, struggling with margin and padding on the h1 and p element

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,20 @@ import fire from './assets/fire.jpg'
 function App() {
 
   return (
-    < >
-  <h1> Portfolio </h1>
-  <p> Håkon Stokkenes </p>
-  <img src={fire}></img> 
+    <div>
+
     <div className="container">
-      <button className="item item-1">Home</button>
-      <button className="item item-2">Projects</button>
-      <button className="item item-3">info</button>
+      <button className="button-1">Home</button>
+      <button className="button-2">Projects</button>
+      <button className="button-3">info</button>
+      <button className="button-4">Contact</button>
     </div>
 
+  <h1> Portfolio </h1>
+  <p> Håkon Stokkenes </p>
+  <img src={fire} className='centerimg'></img> 
 
-    </>
+    </div>
     
   )
   
---
+++ src/pages/Contactpage.tsx
@@ -0,0 +1,15 @@
+
+/*function contactapp() {
+
+    return (
+    < >
+
+        <title> Contact </title>
+
+    <h2> im in a coop with the colla sippin on WAH</h2>
+    
+    </>
+    )
+}
+
+export default contactapp*/
