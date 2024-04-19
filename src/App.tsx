46dede5b95ff7a44c11ec70bda0e00cc8f38835d
Håkon <Haksto@haugnett.no>
2024-04-19 11:04:50 +0200

    added email, gonna make it so you can send an email to me. Added firebase hosting

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import './App.css'
-import fire from './assets/fire.jpg'
+import React from 'react';
+import './App.css';
+import fire from './assets/fire.jpg';
 
 function App() {
-
   return (
     <div>
+      <div className="container">
+        <button className="button-1">Home</button>
+        <button className="button-2">Projects</button>
+        <button className="button-3">info</button>
+        <button className="button-4">Contact</button>
+      </div>
 
-    <div className="container">
-      <button className="button-1">Home</button>
-      <button className="button-2">Projects</button>
-      <button className="button-3">info</button>
-      <button className="button-4">Contact</button>
+      <h1> Portfolio </h1>
+      <p> Håkon Stokkenes </p>
+      <img src={fire} className='centerimg' alt="fire"></img>
+
+      <form>
+        <div>
+          <label htmlFor="email">Email address</label>
+          <input
+            type="email"
+            id="email"
+            aria-describedby="email-help"
+            placeholder="Enter email"
+          />
+          <small id="email-help">Contact me!</small>
+
+          <button type="submit">Submit</button>
+        </div>
+      </form>
     </div>
-
-  <h1> Portfolio </h1>
-  <p> Håkon Stokkenes </p>
-  <img src={fire} className='centerimg'></img> 
-
-    </div>
-    
-  )
-  
+  );
 }
 
-export default App
+export default App;
